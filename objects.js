1715627c53c4b023f0350b833bde898725394196
rijal <jeryzal333@gmail.com>
2022-09-06 02:55:24 +0700

    class methods

--- objects.js
+++ objects.js
@@ -3,6 +3,13 @@ class User {
     this.email = email;
     this.name = name;
   }
+  login() {
+    console.log(this.email, "Just logged in");
+  }
+
+  logout() {
+    console.log(this.email, "Just logged out");
+  }
 }
 
 var userOne = new User("rijal", "rijal@husen.com");

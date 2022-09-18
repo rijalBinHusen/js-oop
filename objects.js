7166645217c78e8375d19bc1e0c2f447fe71fec4
rijal <jeryzal333@gmail.com>
2022-09-18 13:55:51 +0700

    prototype inheritance

--- objects.js
+++ objects.js
@@ -13,5 +13,21 @@ User.prototype.logout = function () {
   console.log(this.email, " has logged out");
 };
 
+function Admin(...args) {
+  User.apply(this, args);
+  this.role = "super admin";
+}
+
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function (user) {
+  users = users.filter((u) => u.name != user.name);
+};
+
 var userOne = new User("rijal", "rijal@husen.com");
 var userTwo = new User("Ibin husen", "ibin@husen.com");
+var admin = new Admin("admin", "admin@admin.com");
+
+var users = [userOne, userTwo, admin];
+
+console.log(admin);

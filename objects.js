dcb9656d2eff52b07b248d85f2ca1b795523f275
rijal <jeryzal333@gmail.com>
2022-09-18 13:06:23 +0700

    method chaining

--- objects.js
+++ objects.js
@@ -2,15 +2,26 @@ class User {
   constructor(email, name) {
     this.email = email;
     this.name = name;
+    this.score = 0;
   }
   login() {
     console.log(this.email, "Just logged in");
+    return this;
   }
 
   logout() {
     console.log(this.email, "Just logged out");
+    return this;
+  }
+  updateScore() {
+    this.score++;
+    console.log(this.email, " score is now: " + this.score);
+    return this;
   }
 }
 
 var userOne = new User("rijal", "rijal@husen.com");
 var userTwo = new User("Ibin husen", "ibin@husen.com");
+
+userOne.login().logout();
+userTwo.logout();

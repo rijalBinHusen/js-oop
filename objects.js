a50ff8f7eb9a12baec2718438ac007206aa08ff5
rijal <jeryzal333@gmail.com>
2022-09-18 13:28:21 +0700

    constructor

--- objects.js
+++ objects.js
@@ -1,38 +1,14 @@
-class User {
-  constructor(email, name) {
-    this.email = email;
-    this.name = name;
-    this.score = 0;
-  }
-  login() {
-    console.log(this.email, "Just logged in");
-    return this;
-  }
-
-  logout() {
-    console.log(this.email, "Just logged out");
-    return this;
-  }
-  updateScore() {
-    this.score++;
-    console.log(this.email, " score is now: " + this.score);
-    return this;
-  }
+function User(name, email) {
+  this.email = email;
+  this.name = name;
+  this.online = false;
+  this.login = function () {
+    console.log(this.email, " has logged in");
+  };
 }
 
 var userOne = new User("rijal", "rijal@husen.com");
 var userTwo = new User("Ibin husen", "ibin@husen.com");
 
-var users = [userOne, userTwo];
-
-class Admin extends User {
-  deleteUser(user) {
-    users = users.filter((u) => u.name != user.name);
-  }
-}
-
-var admin = new Admin("rijal2", "rijal ke dua");
-
-admin.deleteUser(userTwo);
-
-console.log(users);
+console.log(userOne);
+userTwo.login();

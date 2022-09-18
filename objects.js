0fb57ae1a2fdfbb08876eab1193ad4369082ea5e
rijal <jeryzal333@gmail.com>
2022-09-18 13:40:03 +0700

    prototype

--- objects.js
+++ objects.js
@@ -2,13 +2,16 @@ function User(name, email) {
   this.email = email;
   this.name = name;
   this.online = false;
-  this.login = function () {
-    console.log(this.email, " has logged in");
-  };
 }
+
+User.prototype.login = function () {
+  this.online = true;
+  console.log(this.email, " has logged in");
+};
+User.prototype.logout = function () {
+  this.online = false;
+  console.log(this.email, " has logged out");
+};
 
 var userOne = new User("rijal", "rijal@husen.com");
 var userTwo = new User("Ibin husen", "ibin@husen.com");
-
-console.log(userOne);
-userTwo.login();
